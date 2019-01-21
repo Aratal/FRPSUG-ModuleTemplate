2f037624c5770732d548b8a78532ac9c1361fed8
Laurent LIENHARD <laurent.lienhard@outlook.com>
2019-01-21 20:58:54 +0100

    commit

--- CI/03_Build.ps1
+++ CI/03_Build.ps1
@@ -46,12 +46,6 @@ Foreach($file in $MainPSM1Contents){
     
 }
 
-<# Write-Host "[BUILD][START][POST] Adding post content" -ForegroundColor RED -BackgroundColor White
-
-$PostContentPath = Join-Path -Path $Current -ChildPath "03_postContent.ps1"
-$file = Get-item $PostContentPath
-Get-Content $File.FullName | out-File -FilePath $ExportPath -Encoding utf8 -Append #>
-
 Write-Host "[BUILD][START][PSD1] Adding functions to export" -ForegroundColor RED -BackgroundColor White
 
 $FunctionsToExport = $PublicFunctions.BaseName
