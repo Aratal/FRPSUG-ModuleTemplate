151cf9533daad3a53b88519c912982bd1f4fa9fa
Laurent LIENHARD <laurent.lienhard@outlook.com>
2019-02-03 12:00:52 +0100

    add psake script

--- CI/02_Install.ps1
+++
@@ -1,8 +0,0 @@
-
-write-host "[INSTALL] Start" -ForegroundColor Red -BackgroundColor White
-write-host "[INSTALL] Install core modules" -ForegroundColor Red -BackgroundColor White
-Install-Module -Name PSScriptAnalyzer -Force  -Scope CurrentUser
-Install-Module -Name Pester -Force  -Scope CurrentUser
-Install-Module -Name PSake -Force  -Scope CurrentUser
-Install-Module -Name PSDeploy -Force  -Scope CurrentUser
-write-host "[INSTALL] End" -ForegroundColor Red -BackgroundColor White
